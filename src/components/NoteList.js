0ff7954494104e93356ea0abd09ea189c57a395f
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-10 13:20:54 +0300

    regular commit18

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,3 +1,15 @@
 import React from "react";
 import "./styles/list.css";
+import Header from "./list-comps/Header"
+import NoteRow from "./list-comps/NoteRow"
+import AddButton from "./list-comps/AddButton"
 
+export default function NoteList(){
+
+
+    return(
+        <div className="note-list-main">
+
+        </div>
+    )
+}
