11f3656f7ec4a0f8d7c1ad797e56e4e3b767a6f9
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-10 21:57:59 +0300

    regular commit225

--- src/components/editors-comps/EditHeader.js
+++ src/components/editors-comps/EditHeader.js
@@ -3,13 +3,15 @@ import React from "react"
 export default function EditHeader(){
     return(
         <div className="editor-header">
-            <button id="save" className="editor-button">
-                Save
-            </button>
-            <span>Note Editor</span>
-            <button id="back" className="editor-button">
-                Back
-            </button>
+            <div className="editor-header-cont">
+                <button id="save" className="editor-button">
+                    Save
+                </button>
+                <span>Note Editor</span>
+                <button id="back" className="editor-button">
+                    Back
+                </button>
+            </div>
         </div>
     )
 }
