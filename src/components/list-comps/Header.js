dda5e010e2f9940023197b8350c944bffa638d68
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-10 15:43:02 +0300

    regular commit153

--- src/components/list-comps/Header.js
+++ src/components/list-comps/Header.js
@@ -5,7 +5,6 @@ export default function Header(){
         let header = document.getElementsByClassName("header-main")[0];
 
         if ((window.pageYOffset - header.offsetHeight) > header.offsetTop){
-            header.style.transform = `translateY(${header.offsetHeight}px)`
             header.classList.add("sticky-header")
         }else{
             header.classList.remove("sticky-header")
