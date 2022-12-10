6d819e2b9ee1bfef889604a325d0b1da6e7ace5b
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-10 13:25:10 +0300

    regular commit22

---
+++ src/components/list-comps/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default Header(){
+    return(
+        <div className="header-main">
+            <div className="search-bar">
+                <i className="search-icon">
+
+                </i>
+
+                <input type="text" />
+            </div>
+        </div>
+    )
+}
