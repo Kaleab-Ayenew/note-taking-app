28ab2bbf07654a4dd7cef72b2306cd112342d9f9
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-10 15:01:49 +0300

    regular commit113

--- src/components/list-comps/NoteRow.js
+++ src/components/list-comps/NoteRow.js
@@ -8,10 +8,8 @@ export default function NoteRow(){
                     This is a title
                 </h2>
                 <p className="note-text">
-                    This is some randome note content that nobody cares about,
-                    I also don't care This is some randome note content that nobody cares about,
-                    I also don't careThis is some randome note content that nobody cares about,
-                    I also don't care
+                    This is some randome note content that nobody cares about, This is some randome note content that nobody cares about
+                    
                 </p>
             </div>
         </div>
