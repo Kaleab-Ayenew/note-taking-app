7a5c7c21460a45f9c58e5a85a241d995a97ad701
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-10 21:52:40 +0300

    regular commit220

--- src/components/editors-comps/EditHeader.js
+++ src/components/editors-comps/EditHeader.js
@@ -3,7 +3,10 @@ import React from "react"
 export default function EditHeader(){
     return(
         <div className="editor-header">
-            
+            <div className="editor-button">
+                Save
+            </div>
+            <span>Note Editor</span>
         </div>
     )
 }
