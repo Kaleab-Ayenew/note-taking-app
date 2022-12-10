89199cf7d32530735b4cbafcba6159414527caa4
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-10 14:49:09 +0300

    regular commit101

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -15,6 +15,12 @@ export default function NoteList(){
                 <NoteRow />
                 <NoteRow />
                 <NoteRow />
+                <NoteRow />
+                <NoteRow />
+                <NoteRow />
+                <NoteRow />
+                <NoteRow />
+                <NoteRow />
             </div>
 
             <div className="add-button">
