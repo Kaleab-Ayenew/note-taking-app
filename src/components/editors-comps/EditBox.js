840287f3fefd2422c81e0757f5aa3505ca21853d
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-10 22:05:25 +0300

    regular commit232

--- src/components/editors-comps/EditBox.js
+++ src/components/editors-comps/EditBox.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function EditBox(){
     return (
-        <div>
-            This is an empty div
+        <div className="editor-box">
+            
         </div>
     )
 }
