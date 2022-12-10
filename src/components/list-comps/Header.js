55bd26af696ba5f79e63fb97c6e53a7abbe226d1
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-10 13:48:43 +0300

    regular commit44

--- src/components/list-comps/Header.js
+++ src/components/list-comps/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import searchIcon from '../../images/search-icon.png'
 export default function Header(){
     return (
         <div className="header-main">
             <div className="search-bar">
                 <i className="search-icon">
+                    <img src={searchIcon} alt="search-icon"/>
                 </i>
 
                 <input className="search-bar-input" type="text" />
