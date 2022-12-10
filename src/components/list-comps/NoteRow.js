d3b8aa61c75696cdf203da16de3b41aba20a549b
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-10 14:27:56 +0300

    regular commit81

--- src/components/list-comps/NoteRow.js
+++ src/components/list-comps/NoteRow.js
@@ -3,13 +3,15 @@ import React from "react";
 export default function NoteRow(){
     return (
         <div className="note-row">
-            <h2 className="note-title">
-                This is a title
-            </h2>
-            <p className="note-text">
-                This is some randome note content that nobody cares about,
-                I also don't care
-            </p>
+            <div className="note-row-content">
+                <h2 className="note-title">
+                    This is a title
+                </h2>
+                <p className="note-text">
+                    This is some randome note content that nobody cares about,
+                    I also don't care
+                </p>
+            </div>
         </div>
     )
 }
