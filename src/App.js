4122223945f805fa40a094940af021abf8190a17
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2022-12-21 14:32:37 +0300

    Changed the navigation to router

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import Login from "./components/Login"
 import React from 'react'
 import Signup from './components/SignUp';
 import SignUpDone from './components/SignUpDone';
+import Main from './Main';
+
+import { Outlet, Route, Routes } from 'react-router';
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+
 
 function App() {
   let [activeComp, setActiveComp] = React.useState({"name":"login","props":{}}) //Possible values are editor, list, login, signup
@@ -20,13 +27,64 @@ function App() {
   }
   
   let comp = compObj[activeComp.name];
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main />,
+      loader: null,
+      action: null,
+      errorElement: null,
+      children: [
+        {
+          index: true,
+          element: <NoteList data={activeComp.props} url={url} setUrl={setUrl} userInfo={userInfo} setUserInfo={setUserInfo} activeComp={activeComp} setActiveComp={setActiveComp}/>,
+          loader: null,
+          action: null,
+          errorElement: null
+        },
+        {
+          path: "home",
+          element: <NoteList data={activeComp.props} url={url} setUrl={setUrl} userInfo={userInfo} setUserInfo={setUserInfo} activeComp={activeComp} setActiveComp={setActiveComp}/>,
+          loader: null,
+          action: null,
+          errorElement: null
+        },
+        ,
+        {
+          path: "login",
+          element: <Login />,
+          loader: null,
+          action: null,
+          errorElement: null
+        },
+        {
+          path: "sign-up",
+          element: <Signup />,
+          loader: null,
+          action: null,
+          errorElement: null
+        },
+        {
+          path: "editor",
+          element: <Editor />,
+          loader: null,
+          action: null,
+          errorElement: null
+        },
+        {
+          path: "sign-up-success",
+          element: <SignUpDone />,
+          loader: null,
+          action: null,
+          errorElement: null
+        },
+      ]
+    }
+  ])
   
   return (
-    <div className="App">
-
-      {comp}
-
-    </div>
+    <RouterProvider router={router}/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { routerList } from './App';
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
 );
 
 
